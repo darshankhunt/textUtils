14041b089fdbc716980e06a02286998af86b3611
darshankhunt <darshankhunt13@gmail.com>
2023-01-05 23:14:23 +0530

    modify app using bootstrap

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,14 +12,30 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
-    const [Text, setText] = useState('Enter Text Here');
+    const handleloClick = ()=>{
+        let newText1 = Text.toLowerCase();
+        setText(newText1);
+    }
+
+    const [Text, setText] = useState('');
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value={Text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="10"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleloClick}>Convert to lowercase</button>
         </div>
+        <div className="container my-3">
+            <h2>Your Text summary</h2>
+            <p>{Text.split(" ").length} words, {Text.length} Characters</p>
+            <p>{0.008*Text.split(" ").length} Minutes Read</p>
+            <h2>Preview</h2>
+            <p>{Text}</p>
+
+        </div>
+        </>
     )
 }
